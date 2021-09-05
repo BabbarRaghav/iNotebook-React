4f6b5cbcf279bd1d01d34263027bce27b9db7ba4
Raghav <babbarraghav2@gmail.com>
2021-09-05 23:52:11 +0530

    Adding Navbar and router in Reactjs

--- src/App.js
+++ src/App.js
@@ -1,10 +1,28 @@
 import './App.css';
+import { Navbar } from './component/Navbar';
+import { Home } from './component/Home';
+import { About } from './component/About';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route
+} from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <h1>This is inotebook</h1>
-    </div>
+    <>
+      <Router>
+        <Navbar />
+        <Switch>
+          <Route exact path="/about">
+            <About />
+          </Route>
+          <Route exact path="/">
+            <Home />
+          </Route>
+        </Switch>
+      </Router>
+    </>
   );
 }
 
